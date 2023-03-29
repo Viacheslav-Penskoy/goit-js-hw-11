2a00a30c456860e4abe24a70a837105c0ac0b070
Viacheslav-Penskoy <114249044+Viacheslav-Penskoy@users.noreply.github.com>
2023-03-29 18:16:24 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { fetchImages } from './fetchImages';
+
 import Notiflix from 'notiflix';
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
